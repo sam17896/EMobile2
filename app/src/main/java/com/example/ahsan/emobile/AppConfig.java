666dec72a0c34ace27c3d9ce376c43a3b84e827c
Ahsan Sohail <k142109@nu.edu.pk>
2017-05-04 11:14:21 +0500

    logo changed

--- app/src/main/java/com/example/ahsan/emobile/AppConfig.java
+++ app/src/main/java/com/example/ahsan/emobile/AppConfig.java
@@ -2,6 +2,6 @@ package com.example.ahsan.emobile;
 
 
 public class AppConfig {
-    public static String URL = "http://192.168.1.8:8080/e-discussionmobile/";
-    public static String IMAGESURL = "http://192.168.1.8:8080/e-discussionmobile/uploaded_files/";
+    public static String URL = "http://192.168.43.131:8080/e-discussionmobile/";
+    public static String IMAGESURL = "http://192.168.43.131:8080/e-discussionmobile/uploaded_files/";
 }
